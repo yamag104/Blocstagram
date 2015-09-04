5738f232e8dab09162f69ded53863fdf4e2999df
Yoko Yamaguchi <yamag104@mail.chapman.edu>
2015-09-04 13:58:18 -0700

    Removed storyboard

---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Yoko Yamaguchi on 9/4/15.
+//  Copyright (c) 2015 Yoko Yamaguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
